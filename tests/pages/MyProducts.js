ea4f469401de9ea4445ee45299dbc647b10c9ae3
Rahat Chowdhury <rczisan@gmail.com>
2026-01-01 19:17:06 +0600

    Updated 'Create' and 'Read' portion of the CRUD

--- tests/pages/MyProducts.js
+++ tests/pages/MyProducts.js
@@ -33,4 +33,74 @@ export class MyProductsPage extends BasePage {
             console.log("Couldn't navigate to 'My Products' page");
         }
     }
+
+    async navigateToAddProductPage() {
+        await this.page.getByText('Add Product').click();
+    }
+
+    async checkAddProductNavigationSuccess() {
+        const isAddProductVisible = await this.page.getByRole('heading', { name: 'ADD PRODUCT' }).isVisible();
+        if (isAddProductVisible) {
+            console.log("Navigated to 'Add Product' page successfully");
+        } else {
+            console.log("Couldn't navigate to 'Add Product' page");
+        }
+    }
+
+    async createNewProduct(productName, productDescription, purchasePrice, rentPrice) {
+        // product title
+        await this.page.locator('input[name="title"]').fill(productName);
+        
+        // product category
+        await this.page.getByRole('listbox').filter({ hasText: 'Sporting' }).click();
+        await this.page.getByRole('option', { name: 'Electronics' }).click();
+
+        // product description, purchase price, rent price
+        await this.page.locator('textarea[name="description"]').fill(productDescription);
+        await this.page.locator('input[name="purchase_price"]').fill(purchasePrice);
+        await this.page.locator('input[name="rent_price"]').fill(rentPrice);
+
+        // product rent terms
+        await this.page.getByRole('listbox').filter({ hasText: 'DailyHourlyWeeklyMonthlyAnnually' }).click();
+        await this.page.getByRole('option', { name: 'Daily' }).click();
+        
+        // Click on "Add Product" button
+        await this.page.click('button:has-text("Add Product")');
+    }
+
+    // Verify product creation success
+    async checkProductCreationSuccess() {
+        const successMessage = await this.page.getByText('New Product added!').isVisible();
+        if (successMessage) {
+            console.log("Product was created successfully");
+        } else {
+            console.log("Product creation failed");
+        }
+    }
+
+    // verify newly added product is visible in My Products list
+    async viewNewlyAddedProduct(productName) {
+        const isNewlyAddedProductVisible = await this.page.getByText(productName).first().isVisible();
+        if (isNewlyAddedProductVisible) {
+            console.log("Newly added product is clearly visible");
+        } else {
+            console.log("Newly added product is not visible");
+        }
+    }
+
+    // View newly added product details
+    async clickOnSpecificProduct(productName) {
+        await this.page.getByText(productName).first().click();
+    }
+
+    async checkProductDetailsDisplayed(productName) {
+        const isTitleVisible = await this.page.getByRole('heading', { name: 'EDIT PRODUCT' }).isVisible();
+        const isProductNameVisible = await this.page.locator('input[name="title"]').inputValue() === productName;
+
+        if (isTitleVisible && isProductNameVisible) {
+            console.log("Product details are displayed");
+        } else {
+            console.log("Product details are not displayed");
+        }
+    }
 }
--- tests/products/productCRUD.spec.js
+++ tests/products/productCRUD.spec.js
@@ -1,16 +1,29 @@
-import { ProductListPage } from "../pages/ProductListPage";
+import { MyProductsPage } from "../pages/MyProducts";
 import { test } from "@playwright/test";
 
 test.describe('CRUD Tests', () => {
     test('Create, Read, Update, Delete Products', async ({ page }) => {
-        const productListPage = new ProductListPage(page);
-        await productListPage.navigate();
-        await productListPage.loginFirst();
-        await productListPage.checkLoginSuccess();
-        
-        await productListPage.navigateToProductListPage();
-        await productListPage.checkNavigationSuccess();
+        const myProductsPage = new MyProductsPage(page);
+        await myProductsPage.navigate();
+        await myProductsPage.loginFirst();
+        await myProductsPage.checkLoginSuccess();
 
+        await myProductsPage.navigateToMyProductsPage();
+        await myProductsPage.checkMyProductsNavigationSuccess();
         
+        // Navigate to Add Product Page
+        await myProductsPage.navigateToAddProductPage();
+        await myProductsPage.checkAddProductNavigationSuccess();
+
+        // Create a new product
+        await myProductsPage.createNewProduct("Test Product", "This is a test product", "19.99", "9.99");
+        await myProductsPage.checkProductCreationSuccess();
+
+        // Read product Card
+        await myProductsPage.viewNewlyAddedProduct("Test Product");
+
+        // Read product details
+        await myProductsPage.clickOnSpecificProduct("Test Product");
+        await myProductsPage.checkProductDetailsDisplayed("Test Product");
     });
 });
