afec7c69390d92cdc1446133fbefe4b25e474e5d
Rahat Chowdhury <rczisan@gmail.com>
2026-01-01 20:19:22 +0600

    Added the 'Buy' and 'Rent' part

--- tests/pages/ProductDetailsPage.js
+++ tests/pages/ProductDetailsPage.js
@@ -21,12 +21,76 @@ export class ProductDetailsPage extends BasePage {
         await this.page.getByText(productName).click();
     }
 
-    async checkProductDetailsDisplayed() {
-        const productDetails = await this.page.getByText('Product Details').isVisible();
-        if (productDetails) {
+    async checkProductDetailsDisplayed(productName) {
+        const isTitleVisible = await this.page.getByText('Product Details').isVisible();
+        const isProductNameVisible = await this.page.getByText(productName).isVisible();
+        if (isTitleVisible && isProductNameVisible) {
             console.log("Product details are displayed");
         } else {
             console.log("Product details are not displayed");
+        }
+    }
+
+    // Rent option visibility and functionality
+    async chooseRentOptionAvailability() {
+        const isRentButtonVisible = await this.page.getByRole('button', { name: 'Rent' }).isVisible();
+        isRentButtonVisible ? console.log("Rent option is available") : console.log("Rent option is not available");
+    }
+
+    async clickOnRentButton() {
+        await this.page.getByRole('button', { name: 'Rent' }).click();
+    }
+
+    async checkRentFormDisplayed() {
+        const isModalVisible = await this.page.getByText("Start date").isVisible();
+        isModalVisible ? console.log("Rent form is displayed") : console.log("Rent form is not displayed");
+    }
+
+    async fillUpRentInfo() {
+        await this.page.locator('input[name="start_date"]').fill('2024-07-01');
+        await this.page.locator('input[name="end_date"]').fill('2024-07-10');
+    }
+
+    async submitRentRequest() {
+        await this.page.getByRole('button', { name: 'Book rent' }).click();
+    }
+
+    async checkRentRequestSuccess(productName) {
+        const isProductNameVisible = await this.page.getByText(productName).isVisible();
+        const rentHistoryText = await this.page.getByText('Rent History: (Aug 1st 2024 - Aug 10th 2024)').isVisible();
+        if (isProductNameVisible && rentHistoryText) {
+            console.log("Rent request successful");
+        } else {
+            console.log("Rent request failed");
+        }
+    }
+
+
+    // Buy option visibility and functionality
+    async chooseBuyOptionAvailability() {
+        const isBuyButtonVisible = await this.page.getByRole('button', { name: 'Buy' }).isVisible();
+        isBuyButtonVisible ? console.log("Buy option is available") : console.log("Buy option is not available");
+    }
+
+    async clickOnBuyButton() {
+        await this.page.getByRole('button', { name: 'Buy' }).click();
+    }
+
+    async confirmBuyAction() {
+        await this.page.getByRole('button', { name: 'Yes!' }).click();
+    }
+
+    async checkBuyRequestSuccess(productName) {
+        const isProductNameVisible = await this.page.getByText(productName).isVisible();
+        const isTitleVisible = await this.page.getByRole('heading', { name: 'PRODUCT DETAILS' }).isVisible();
+        const isBuyButtonVisible = await this.page.getByRole('button', { name: 'Buy' }).isVisible();
+        
+        if (isTitleVisible) {
+            if (isProductNameVisible && !isBuyButtonVisible) {
+                console.log("Buy request successful");
+            } else {
+                console.log("Buy request failed");
+            }
         }
     }
 };
---
+++ tests/products/productRentBuy.spec.js
@@ -0,0 +1,34 @@
+import {test} from "@playwright/test";
+import { ProductDetailsPage } from "../pages/ProductDetailsPage";
+
+test.describe('Product Rent/Buy Options', () => {
+    test('View Product Rent/Buy Options', async ({ page }) => {
+        const productDetailsPage = new ProductDetailsPage(page);
+        await productDetailsPage.navigate();
+        await productDetailsPage.loginFirst();
+        
+        await productDetailsPage.navigateToProductListPage();
+
+        const specificProductName = "Funshine bear";
+        await productDetailsPage.clickOnSpecificProduct(specificProductName);
+        await productDetailsPage.checkProductDetailsDisplayed(specificProductName);
+
+        // Check Rent Option
+        await productDetailsPage.chooseRentOptionAvailability();
+        
+        // If Rent option is available, proceed to test rent functionality
+        await productDetailsPage.clickOnRentButton();
+        await productDetailsPage.checkRentFormDisplayed();
+        await productDetailsPage.fillUpRentInfo();
+        await productDetailsPage.submitRentRequest();
+        await productDetailsPage.checkRentRequestSuccess(specificProductName);
+
+        // Check Buy Option
+        await productDetailsPage.chooseBuyOptionAvailability();
+        
+        // If Buy option is available, proceed to test buy functionality
+        await productDetailsPage.clickOnBuyButton();
+        await productDetailsPage.confirmBuyAction();
+        await productDetailsPage.checkBuyRequestSuccess(specificProductName);
+    });
+});
